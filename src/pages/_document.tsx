0484a120f15405613cadbb78f7de201148716550
hasnalathifah <hasna.lathifah394@gmail.com>
2024-03-27 14:24:02 +0700

    update ar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,20 @@
-import { Html, Head, Main, NextScript } from "next/document";
+/* eslint-disable @next/next/no-sync-scripts */
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <script src='https://aframe.io/releases/1.3.0/aframe.min.js'></script>
+        <script
+          type='text/javascript'
+          src='https://raw.githack.com/AR-js-org/AR.js/master/three.js/build/ar-threex-location-only.js'
+        ></script>
+        <script
+          type='text/javascript'
+          src='https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js'
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
