9d8194f2947259cd6be8d217dc954b16af76d84c
hasnalathifah <hasna.lathifah394@gmail.com>
2024-03-27 13:25:41 +0700

    update ar

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    dirs: ['src'],
-  },
-
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
-  swcMinify: true,
-
-  // Uncoment to add domain whitelist
-  images: {
-    domains: ['hypeandplay.my.id'],
-  },
-
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
@@ -21,24 +10,6 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
 
-  // SVGR
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            typescript: true,
-            icon: true,
-          },
-        },
-      ],
-    });
-
-    return config;
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,26 +1,26 @@
 /* eslint-disable @next/next/no-sync-scripts */
 import { Head, Html, Main, NextScript } from 'next/document';
 
-const preloadFont = [
-  '/fonts/Cal-Sans/CalSans-SemiBold.woff2',
+// const preloadFont = [
+//   '/fonts/Cal-Sans/CalSans-SemiBold.woff2',
 
-  '/fonts/Matter/Matter-Regular.woff2',
-  '/fonts/Matter/Matter-RegularItalic.woff2',
-  '/fonts/Matter/Matter-Medium.woff2',
-  '/fonts/Matter/Matter-MediumItalic.woff2',
-  '/fonts/Matter/Matter-SemiBold.woff2',
-  '/fonts/Matter/Matter-SemiBoldItalic.woff2',
-  '/fonts/Matter/Matter-Bold.woff2',
-  '/fonts/Matter/Matter-BoldItalic.woff2',
-  '/fonts/Matter/Matter-Heavy.woff2',
-  '/fonts/Matter/Matter-Heavy.woff2',
-];
+//   '/fonts/Matter/Matter-Regular.woff2',
+//   '/fonts/Matter/Matter-RegularItalic.woff2',
+//   '/fonts/Matter/Matter-Medium.woff2',
+//   '/fonts/Matter/Matter-MediumItalic.woff2',
+//   '/fonts/Matter/Matter-SemiBold.woff2',
+//   '/fonts/Matter/Matter-SemiBoldItalic.woff2',
+//   '/fonts/Matter/Matter-Bold.woff2',
+//   '/fonts/Matter/Matter-BoldItalic.woff2',
+//   '/fonts/Matter/Matter-Heavy.woff2',
+//   '/fonts/Matter/Matter-Heavy.woff2',
+// ];
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        {preloadFont.map((link) => (
+        {/* {preloadFont.map((link) => (
           <link
             rel='preload'
             key={link}
@@ -29,7 +29,7 @@ export default function Document() {
             type='font/woff2'
             crossOrigin='anonymous'
           />
-        ))}
+        ))} */}
         <script src='https://aframe.io/releases/1.3.0/aframe.min.js'></script>
         <script
           type='text/javascript'
