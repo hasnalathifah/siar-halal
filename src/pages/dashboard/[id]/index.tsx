7183d1fe8973dfa00052f3f05ca0cc02c0585699
hasnalathifah <hasna.lathifah394@gmail.com>
2024-03-29 12:15:53 +0700

    update ar

--- src/pages/dashboard/[id]/index.tsx
+++ src/pages/dashboard/[id]/index.tsx
@@ -97,7 +97,7 @@ export default function Items() {
                                 <p className="mb-1 break-words  font-light text-gray-700 dark:text-gray-400">{alamat}</p>
                             </div>
                         </Link>
-                        <Map/>
+                        {/* <Map/> */}
                         <button type="button" className=" mt-8 relative w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-8 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Mulai navigasi</button>
                         <Link href="/dashboard" type="button" className=" text-center relative w-full text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-8 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800">Batalkan</Link>
                     </div>
