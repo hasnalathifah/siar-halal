4153ec2f0b24dd5df7068d69dd7c47d1f22cb622
hasnalathifah <hasna.lathifah394@gmail.com>
2024-04-18 20:36:17 +0700

    update

--- src/pages/dashboard/[id]/index.tsx
+++ src/pages/dashboard/[id]/index.tsx
@@ -6,6 +6,8 @@ import { useSearchParams } from 'next/navigation'
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { json } from "stream/consumers";
+import { Typography } from "@material-tailwind/react";
+import { Card, CardBody } from "@material-tailwind/react";
 
 // function getlocation() {
 //     navigator.geolocation.getCurrentPosition((position) => {
@@ -125,7 +127,7 @@ export default function Items() {
         <div className=" bg-blue-gray-50">
             <Sidebar/>
             <div className="relative min-w-full mt-8 grid place-items-start justify-center gap-2 ">
-                <div className="divide-y divide-blue-gray-100">
+                <div className="divide-y place-items-center divide-blue-gray-100">
                     <div className="relative grid place-items-start justify-center gap-2">
                         <input value={"Perum ITS Jl. Teknik Sipil W20"} type="text" id="disabled_standard" className="block w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " disabled />
                         <label htmlFor="disabled_standard" className="relative text-sm text-black dark:text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Lokasi asal</label>
@@ -137,11 +139,11 @@ export default function Items() {
                             </div>
                         </Link>
                         <Map/>
-                        <Link href={{pathname: "/ar", query: {dLat,dLon}}} type="button" className=" mt-8 relative w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-8 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Mulai navigasi</Link>
+                        <Link href={{pathname: "/ar", query: {dLat,dLon}}} type="button" className=" text-center mt-8 relative w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-8 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Mulai navigasi</Link>
                         <Link href="/dashboard" type="button" className=" text-center relative w-full text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-8 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800">Batalkan</Link>
                     </div>
-                    <div className="relative w-full place-items-start justify-center mt-1">
-                        <div className=" mt-7 w-full">
+                    <div className="relative grid place-items-start justify-center  mt-1">
+                        <div className=" mt-7 mb-7 w-full">
                             <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
                                 <div className="flex items-center justify-between mb-4">
                                     <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Makanan di Resto ini</h5>
@@ -266,6 +268,111 @@ export default function Items() {
                                         </ul>
                                 </div>
                             </div>
+
+                        </div>
+                    </div>
+                    <div className="relative grid place-items-start justify-center  mt-1">
+                        <div className=" mt-2 mb-7 w-full">
+                            <div className=" relative mt-6 p-4 w-full flex flex-row items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl">
+                            <Card
+                                shadow={false}
+                                color="transparent"
+                                className="grid items-center gap-6 " placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}
+                            >
+                                <CardBody className="p-0 gap-5 flex " placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+                                <div className=" !m-0 h-full  w-full  max-h-[40px] max-w-[40px] ">
+                                    <Image
+                                    width={768}
+                                    height={768}
+                                    src={'/image/food-bg.jpg'}
+                                    alt="img"
+                                    className="h-full rounded w-full object-cover object-center"
+                                    />
+                                </div>
+                                <div>
+                                    <div className="flex gap-1 mb-3 items-center">
+                                    <Typography
+                                        variant="small"
+                                        className=" font-bold flex items-center gap-2 !text-gray-900" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}
+                                    >
+                                        Hasna Lathifah
+                                    </Typography>
+                                    <Typography variant="small" className="font-medium !text-gray-500" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+                                        2 hours ago
+                                    </Typography>
+                                    </div>
+                                    <div className="flex gap-1 mb-3 items-center">
+                                    <table>
+                                        <tbody>
+                                        <tr>
+                                            <th>
+                                            <Typography className="w-full font-normal !text-gray-500" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+                                                Makanan
+                                            </Typography>
+                                            </th>
+                                            <td className="px-6">
+                                            <div className="rating">
+                                                <input disabled type="radio" name="makanan" className="mask mask-star-2 bg-orange-400" />
+                                                <input disabled type="radio" name="makanan" className="mask mask-star-2 bg-orange-400" />
+                                                <input disabled type="radio" name="makanan" className="mask mask-star-2 bg-orange-400" />
+                                                <input disabled type="radio" name="makanan" className="mask mask-star-2 bg-orange-400" checked />
+                                                <input disabled type="radio" name="makanan" className="mask mask-star-2 bg-orange-400" />
+                                            </div>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th>
+                                            <Typography className="w-full font-normal !text-gray-500" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+                                                Pelayanan
+                                            </Typography>
+                                            </th>
+                                            <td className="px-6">
+                                            <div className="rating">
+                                                <input disabled type="radio" name="Pelayanan" className="mask mask-star-2 bg-orange-400" />
+                                                <input disabled type="radio" name="Pelayanan" className="mask mask-star-2 bg-orange-400" />
+                                                <input disabled type="radio" name="Pelayanan" className="mask mask-star-2 bg-orange-400" />
+                                                <input disabled type="radio" name="Pelayanan" className="mask mask-star-2 bg-orange-400" />
+                                                <input disabled type="radio" name="Pelayanan" className="mask mask-star-2 bg-orange-400" checked/>
+                                            </div>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th>
+                                            <Typography className="w-full font-normal !text-gray-500" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+                                                Kebersihan
+                                            </Typography>
+                                            </th>
+                                            <td className="px-6">
+                                            <div className="rating">
+                                                <input disabled type="radio" name="kebersihan" className="mask mask-star-2 bg-orange-400" />
+                                                <input disabled type="radio" name="kebersihan" className="mask mask-star-2 bg-orange-400" />
+                                                <input disabled type="radio" name="kebersihan" className="mask mask-star-2 bg-orange-400" checked />
+                                                <input disabled type="radio" name="kebersihan" className="mask mask-star-2 bg-orange-400" />
+                                                <input disabled type="radio" name="kebersihan" className="mask mask-star-2 bg-orange-400" />
+                                            </div>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                    </div>
+                                    <div className="flex gap-1 mt-8 mb-3 items-center">  
+                                    <Typography className="w-full font-normal mb-4 !text-gray-500" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                    </Typography>          
+                                    </div>
+                                    <div className="flex gap-1 items-center">              
+                                        <Image
+                                        width={200}
+                                        height={200}
+                                        src={'/image/food.jpg'}
+                                        alt="img"
+                                        className="h-full rounded w-200 object-cover object-center"
+                                        />
+                                    </div>
+                                </div>
+                                </CardBody>
+                            </Card>
+                        </div>
 
                         </div>
                     </div>
