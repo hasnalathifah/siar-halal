f67dca768422ae274658b8bc2087e663292ab657
hasnalathifah <hasna.lathifah394@gmail.com>
2024-04-24 11:35:30 +0700

    update ar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,5 +4,6 @@ import type { AppProps } from "next/app";
 import {lookAt} from '@/components/LookAt/look-at'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+  return <Component {...pageProps}{...lookAt} />;
 }
