c23f6b9a7e3401f5b15e19e1e78a30353e42e240
hasnalathifah <hasna.lathifah394@gmail.com>
2024-04-26 16:54:46 +0700

    update ar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -167,6 +167,20 @@ export default function Sidebar() {
               </li>
             </ul>
 
+            <hr className="my-4 md:min-w-full" />
+
+            <ul className="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
+              <li className="items-center">
+                <Link href="/owner"
+                    className="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
+                  >
+                    <i className="fas fa-fingerprint text-blueGray-400 mr-2 text-sm"></i>{" "}
+                    Kelola Resto
+                   
+                </Link>
+              </li>
+            </ul>
+
             {/* Divider */}
             <hr className="my-4 md:min-w-full" />
             {/* Heading */}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,6 +15,7 @@ export default function Document() {
           type='text/javascript'
           src='https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js'
         ></script>
+        <script src="https://unpkg.com/aframe-look-at-component@0.8.0/dist/aframe-look-at-component.min.js"></script> 
        
       </Head>
       <body>
