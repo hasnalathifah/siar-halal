23a26cced4b084fdd989337e35c44bd9e3a05348
hasnalathifah <hasna.lathifah394@gmail.com>
2024-04-30 13:45:22 +0700

    update ar

--- src/pages/ar/index.tsx
+++ src/pages/ar/index.tsx
@@ -162,7 +162,7 @@ if(dist < 100){
 console.log(currlat)
 
   nav.push(
-    <a-entity gps-new-entity-place={"latitude:"+currlat+" ; longitude:"+currlon} position='0 0 10' id="nav" look-at={'#target'+idloc} gltf-model={'#panah'} animation-mixer='loop-repeat' scale={'0.3 0.3 0.3'}>
+    <a-entity gps-new-entity-place={"latitude:"+currlat+" ; longitude:"+currlon} position='10 0 0' id="nav" look-at={'#target'+idloc} gltf-model={'#panah'} animation-mixer='loop-repeat' scale={'0.3 0.3 0.3'}>
         </a-entity>
   )
 
