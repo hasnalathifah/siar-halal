a1129f94515757fa3e856829a6f6753a33de882d
hasnalathifah <hasna.lathifah394@gmail.com>
2024-04-30 12:46:15 +0700

    update ar

--- src/pages/ar/index.tsx
+++ src/pages/ar/index.tsx
@@ -155,7 +155,7 @@ info = <div className=' text-xl text-black'>dist = {decimalAdjust("round", dist,
 
 
 if(dist < 100){
-  finish=<Link href='/dashboard' type='button' className=' justify-center items-centertext-center relative w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-8 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none mt-96'>Selesai</Link>
+  finish=<Link href='/dashboard' type='button' className=' text-center relative w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-8 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none mt-96'>Selesai</Link>
 
 }
 
