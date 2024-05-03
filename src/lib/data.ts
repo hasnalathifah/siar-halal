253d1065dec5a372445f8ef168ee089c3051fae4
hasnalathifah <hasna.lathifah394@gmail.com>
2024-05-03 10:54:18 +0700

    update ar

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -36,8 +36,8 @@ export const resto =[
       id_resto: 5,
       nama_resto: "Testing",
       alamat: "Jl. Teknik Sipil, Keputih, Sukolilo, Surabaya",
-      lat: -7.289868,
-      lon: 112.796230
+      lat: -7.288242,
+      lon: 112.795781
   }
 ]
 
