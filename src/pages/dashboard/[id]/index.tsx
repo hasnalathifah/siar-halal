a9513e0d45162a0b58fc8db1608697ffe1a22f95
hasnalathifah <hasna.lathifah394@gmail.com>
2024-05-17 09:45:24 +0700

    update ar

--- src/pages/dashboard/[id]/index.tsx
+++ src/pages/dashboard/[id]/index.tsx
@@ -177,8 +177,8 @@ export default function Items() {
     let arButton = []
     if (resp.length!=0){
         arButton.push(
-            // <Link href={"https://hadziq.8thwall.app/siarhalal?destlat=" + dLat + "&destlon=" + dLon + "&mode=" + mode} type="button" className=" text-center mt-8 relative w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-8 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Mulai navigasi</Link>
-            <Link href={{pathname: '/ar', query: {str}}} type="button" className=" text-center mt-8 relative w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-8 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Mulai navigasi</Link>
+            <Link href={"https://hadziq.8thwall.app/siarhalal?destlat=" + dLat + "&destlon=" + dLon + "&mode=" + mode} type="button" className=" text-center mt-8 relative w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-8 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Mulai navigasi</Link>
+            // <Link href={{pathname: '/ar', query: {str}}} type="button" className=" text-center mt-8 relative w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-8 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Mulai navigasi</Link>
         )
         console.log(resp)
     }
