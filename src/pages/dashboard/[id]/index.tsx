5d9aa36c25a3b116149e7936371ff4e28b2acefa
hasnalathifah <hasna.lathifah394@gmail.com>
2024-08-17 14:17:59 +0700

    commit

--- src/pages/dashboard/[id]/index.tsx
+++ src/pages/dashboard/[id]/index.tsx
@@ -240,6 +240,7 @@ export default function Items() {
     if (resp.length!=0){
         arButton.push(
             <>
+                {/* <Link href={{pathname: '/ar', query: {str}}} type="button" className=" text-center mt-8 relative w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-4 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Mulai navigasi AR</Link> */}
                 <Link href={{pathname: 'https://siar-halal-ar.vercel.app/ar', query: {str}}} type="button" className=" text-center mt-8 relative w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-4 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Mulai navigasi AR</Link>
                 <Link href={"https://hadziq.8thwall.app/siarhalal?destlat=" + dLat + "&destlon=" + dLon + "&mode=" + mode +'&id_user='+user_id} type="button" className=" text-center relative w-full text-white bg-teal-600 hover:bg-teal-700 focus:ring-4 focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-4 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Mulai navigasi 3D Map</Link>
             </>
